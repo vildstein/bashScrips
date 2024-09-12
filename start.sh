352232719ae840ea3818c838560d21abf4f4da4d
vildstein <ng75@mail.ru>
2024-09-12 17:14:28 +0500

    few qt packges added and gh cli added

--- start.sh
+++ start.sh
@@ -7,11 +7,12 @@ sudo apt update
 sudo apt install snapd
 sudo snap install hello-world
 
-
 sudo snap install spotify
 sudo snap install pycharm-community --classic
 sudo snap install code --classic
 sudo snap install telegram-desktop
+
+#Python
 
 #synaptic
 sudo apt install synaptic
@@ -32,15 +33,18 @@ sudo apt install tilix tmux
 sudo apt install libu2f-udev
 
 #Qt5
-sudo apt install build-essential libgl1-mesa-dev libfontconfig1 mesa-common-dev
-sudo apt-get install libglu1-mesa-dev -y
 sudo apt-get install qt5-default
-sudo apt install -y qtcreator qtbase5-dev qt5-qmake qt5-doc qt5-doc-html qtbase5-doc-html qtbase5-examples qtdeclarative5-examples #cmake
+sudo apt install build-essential libgl1-mesa-dev libqt5serialport5-dev qtbase5-dev qtbase5-gles-dev
+sudo apt-get install libfontconfig1 qttools5-dev qttools5-dev-tools libqt5help5 libqt5designer5
+sudo apt-get install mesa-common-dev libqt5svg5-dev qtmultimedia5-dev
+sudo apt-get install libglu1-mesa-dev -y
+sudo apt install -y qtcreator qtbase5-dev qt5-qmake install qt5-doc qt5-doc-html qtbase5-doc-html qtbase5-examples qtdeclarative5-examples #cmake
 sudo apt-get install qtdeclarative5-dev
 sudo apt-get install qtquickcontrols2-5-dev
 
 #Qt6
 sudo apt install qt6-base-dev qt6-base-private-dev qt6-declarative-dev qt6-declarative-private-dev qt6-tools-dev qt6-tools-private-dev qt6-scxml-dev qt6-documentation-tools libqt6core5compat6-dev qt6-tools-dev-tools qt6-l10n-tools qt6-shader-baker libqt6shadertools6-dev qt6-quick3d-dev qt6-quick3d-dev-tools libqt6svg6-dev libqt6quicktimeline6-dev libqt6serialport6-dev
+
 
 #LLVM Latest
 sudo bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
@@ -60,6 +64,22 @@ cd cmake-3.30.3
 make clean
 cd $curDir
 rm -rf cmake-3.30.3
+
+#Git
+sudo apt install git
+#git gh
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+	&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
+
+sudo apt update
+sudo apt install gh
+
+
 
 #Python
 
